e52d27c000a6b9cf93d2e62e80e041f564b69061
Candoran2 <45334438+Candoran2@users.noreply.github.com>
2022-10-01 21:42:55 +0200

    Changed NiPixelFormat to use generated module instead of pyffi (probably still need to change pixeldata setting and saving though).

--- formats/nif/nimain/niobjects/NiPixelFormat.py
+++ formats/nif/nimain/niobjects/NiPixelFormat.py
@@ -1,6 +1,6 @@
 # START_GLOBALS
 import generated.formats.nif as NifFormat
-import pyffi.formats.dds
+from generated.formats.dds import DdsFile
 # END_GLOBALS
 
 class NiPixelFormat:
@@ -8,47 +8,46 @@ class NiPixelFormat:
 	def save_as_dds(self, stream):
 		"""Save image as DDS file."""
 		# set up header and pixel data
-		data = pyffi.formats.dds.DdsFormat.Data()
-		header = data.header
-		pixeldata = data.pixeldata
+		file = DdsFile()
+		pixeldata = file.pixeldata
 
 		# create header, depending on the format
 		if self.pixel_format in (NifFormat.classes.PixelFormat.PX_FMT_RGB8,
 								 NifFormat.classes.PixelFormat.PX_FMT_RGBA8):
 			# uncompressed RGB(A)
-			header.flags.caps = 1
-			header.flags.height = 1
-			header.flags.width = 1
-			header.flags.pixel_format = 1
-			header.flags.mipmap_count = 1
-			header.flags.linear_size = 1
-			header.height = self.mipmaps[0].height
-			header.width = self.mipmaps[0].width
-			header.linear_size = len(self.pixel_data)
-			header.mipmap_count = len(self.mipmaps)
-			header.pixel_format.flags.rgb = 1
-			header.pixel_format.bit_count = self.bits_per_pixel
+			file.flags.caps = 1
+			file.flags.height = 1
+			file.flags.width = 1
+			file.flags.pixel_format = 1
+			file.flags.mipmap_count = 1
+			file.flags.linear_size = 1
+			file.height = self.mipmaps[0].height
+			file.width = self.mipmaps[0].width
+			file.linear_size = len(self.pixel_data)
+			file.mipmap_count = len(self.mipmaps)
+			file.pixel_format.flags.rgb = 1
+			file.pixel_format.bit_count = self.bits_per_pixel
 			if not self.channels:
-				header.pixel_format.r_mask = self.red_mask
-				header.pixel_format.g_mask = self.green_mask
-				header.pixel_format.b_mask = self.blue_mask
-				header.pixel_format.a_mask = self.alpha_mask
+				file.pixel_format.r_mask = self.red_mask
+				file.pixel_format.g_mask = self.green_mask
+				file.pixel_format.b_mask = self.blue_mask
+				file.pixel_format.a_mask = self.alpha_mask
 			else:
 				bit_pos = 0
 				for i, channel in enumerate(self.channels):
 					mask = (2 ** channel.bits_per_channel - 1) << bit_pos
 					if channel.type == NifFormat.classes.ChannelType.CHNL_RED:
-						header.pixel_format.r_mask = mask
+						file.pixel_format.r_mask = mask
 					elif channel.type == NifFormat.classes.ChannelType.CHNL_GREEN:
-						header.pixel_format.g_mask = mask
+						file.pixel_format.g_mask = mask
 					elif channel.type == NifFormat.classes.ChannelType.CHNL_BLUE:
-						header.pixel_format.b_mask = mask
+						file.pixel_format.b_mask = mask
 					elif channel.type == NifFormat.classes.ChannelType.CHNL_ALPHA:
-						header.pixel_format.a_mask = mask
+						file.pixel_format.a_mask = mask
 					bit_pos += channel.bits_per_channel
-			header.caps_1.complex = 1
-			header.caps_1.texture = 1
-			header.caps_1.mipmap = 1
+			file.caps_1.complex = 1
+			file.caps_1.texture = 1
+			file.caps_1.mipmap = 1
 			if self.pixel_data:
 				# used in older nif versions
 				pixeldata.set_value(self.pixel_data)
@@ -57,26 +56,26 @@ class NiPixelFormat:
 				pixeldata.set_value(''.join(self.pixel_data_matrix))
 		elif self.pixel_format == NifFormat.classes.PixelFormat.PX_FMT_DXT1:
 			# format used in Megami Tensei: Imagine and Bully SE
-			header.flags.caps = 1
-			header.flags.height = 1
-			header.flags.width = 1
-			header.flags.pixel_format = 1
-			header.flags.mipmap_count = 1
-			header.flags.linear_size = 0
-			header.height = self.mipmaps[0].height
-			header.width = self.mipmaps[0].width
-			header.linear_size = 0
-			header.mipmap_count = len(self.mipmaps)
-			header.pixel_format.flags.four_c_c = 1
-			header.pixel_format.four_c_c = pyffi.formats.dds.DdsFormat.FourCC.DXT1
-			header.pixel_format.bit_count = 0
-			header.pixel_format.r_mask = 0
-			header.pixel_format.g_mask = 0
-			header.pixel_format.b_mask = 0
-			header.pixel_format.a_mask = 0
-			header.caps_1.complex = 1
-			header.caps_1.texture = 1
-			header.caps_1.mipmap = 1
+			file.flags.caps = 1
+			file.flags.height = 1
+			file.flags.width = 1
+			file.flags.pixel_format = 1
+			file.flags.mipmap_count = 1
+			file.flags.linear_size = 0
+			file.height = self.mipmaps[0].height
+			file.width = self.mipmaps[0].width
+			file.linear_size = 0
+			file.mipmap_count = len(self.mipmaps)
+			file.pixel_format.flags.four_c_c = 1
+			file.pixel_format.four_c_c = FourCC.DXT1
+			file.pixel_format.bit_count = 0
+			file.pixel_format.r_mask = 0
+			file.pixel_format.g_mask = 0
+			file.pixel_format.b_mask = 0
+			file.pixel_format.a_mask = 0
+			file.caps_1.complex = 1
+			file.caps_1.texture = 1
+			file.caps_1.mipmap = 1
 			if isinstance(self,
 						  NifFormat.classes.NiPersistentSrcTextureRendererData):
 				pixeldata.set_value(
@@ -88,29 +87,29 @@ class NiPixelFormat:
 		elif self.pixel_format in (NifFormat.classes.PixelFormat.PX_FMT_DXT5,
 								   NifFormat.classes.PixelFormat.PX_FMT_DXT5_ALT):
 			# format used in Megami Tensei: Imagine
-			header.flags.caps = 1
-			header.flags.height = 1
-			header.flags.width = 1
-			header.flags.pixel_format = 1
-			header.flags.mipmap_count = 1
-			header.flags.linear_size = 0
-			header.height = self.mipmaps[0].height
-			header.width = self.mipmaps[0].width
-			header.linear_size = 0
-			header.mipmap_count = len(self.mipmaps)
-			header.pixel_format.flags.four_c_c = 1
-			header.pixel_format.four_c_c = pyffi.formats.dds.DdsFormat.FourCC.DXT5
-			header.pixel_format.bit_count = 0
-			header.pixel_format.r_mask = 0
-			header.pixel_format.g_mask = 0
-			header.pixel_format.b_mask = 0
-			header.pixel_format.a_mask = 0
-			header.caps_1.complex = 1
-			header.caps_1.texture = 1
-			header.caps_1.mipmap = 1
+			file.flags.caps = 1
+			file.flags.height = 1
+			file.flags.width = 1
+			file.flags.pixel_format = 1
+			file.flags.mipmap_count = 1
+			file.flags.linear_size = 0
+			file.height = self.mipmaps[0].height
+			file.width = self.mipmaps[0].width
+			file.linear_size = 0
+			file.mipmap_count = len(self.mipmaps)
+			file.pixel_format.flags.four_c_c = 1
+			file.pixel_format.four_c_c = FourCC.DXT5
+			file.pixel_format.bit_count = 0
+			file.pixel_format.r_mask = 0
+			file.pixel_format.g_mask = 0
+			file.pixel_format.b_mask = 0
+			file.pixel_format.a_mask = 0
+			file.caps_1.complex = 1
+			file.caps_1.texture = 1
+			file.caps_1.mipmap = 1
 			pixeldata.set_value(''.join(self.pixel_data_matrix))
 		else:
 			raise ValueError(
 				"cannot save pixel format %i as DDS" % self.pixel_format)
 
-		data.write(stream)
+		file.write(stream)
