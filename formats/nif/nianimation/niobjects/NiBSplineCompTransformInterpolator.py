f7ca4acb1b72c2055f288084b3f72ed1fb3029a0
Candoran2 <45334438+Candoran2@users.noreply.github.com>
2023-01-01 17:06:22 +0100

    Updated NiBSplineCompTransformInterpolator in line with field name changes.

--- formats/nif/nianimation/niobjects/NiBSplineCompTransformInterpolator.py
+++ formats/nif/nianimation/niobjects/NiBSplineCompTransformInterpolator.py
@@ -3,18 +3,18 @@ class NiBSplineCompTransformInterpolator:
 
 	def get_translations(self):
 		"""Return an iterator over all translation keys."""
-		return self._getCompKeys(self.translation_offset, 3,
-								 self.translation_bias, self.translation_multiplier)
+		return self._getCompKeys(self.translation_handle, 3,
+								 self.translation_offset, self.translation_half_range)
 
 	def get_rotations(self):
 		"""Return an iterator over all rotation keys."""
-		return self._getCompKeys(self.rotation_offset, 4,
-								 self.rotation_bias, self.rotation_multiplier)
+		return self._getCompKeys(self.rotation_handle, 4,
+								 self.rotation_offset, self.rotation_half_range)
 
 	def get_scales(self):
 		"""Return an iterator over all scale keys."""
-		for key in self._getCompKeys(self.scale_offset, 1,
-									 self.scale_bias, self.scale_multiplier):
+		for key in self._getCompKeys(self.scale_handle, 1,
+									 self.scale_offset, self.scale_half_range):
 			yield key[0]
 
 	def apply_scale(self, scale):
